90125acad52ba1a70e9ce777f6cbb5b47791c9e9
Abhishek8287 <singhabhishek70536@gmail.com>
2024-07-02 13:43:07 +0530

    Hosted backend on the server

--- Client/src/Constant.js
+++ Client/src/Constant.js
@@ -1,4 +1,4 @@
 export const APP_NAME = "MOCK";
 
 export const APP_URL = "http://127.0.0.1:5173/";
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = `https://vcb-ag4z.onrender.com/`;
